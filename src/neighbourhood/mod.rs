d02ed11d1c80327729f2f0a443b13e93349a3ff6
lalitm1004 <lalitm1004@gmail.com>
2025-01-06 22:04:20 +0530

    refactor(neighbourhood): move direction enum to its own module

--- src/neighbourhood/mod.rs
+++ src/neighbourhood/mod.rs
@@ -1,31 +1,5 @@
-#[derive(Debug, PartialEq, Clone, Copy)]
-#[repr(u8)]
-pub enum Direction {
-    Up,
-    TopRight,
-    Right,
-    BottomRight,
-    Down,
-    BottomLeft,
-    Left,
-    TopLeft,
-}
-
-impl Direction {
-    #[inline]
-    pub const fn to_offset(self) -> (i32, i32) {
-        match self {
-            Direction::Up => (0, -1),
-            Direction::TopRight => (1, -1),
-            Direction::Right => (1, 0),
-            Direction::BottomRight => (1, 1),
-            Direction::Down => (0, 1),
-            Direction::BottomLeft => (-1, 1),
-            Direction::Left => (-1, 0),
-            Direction::TopLeft => (-1, -1),
-        }
-    }
-}
+pub mod direction;
+use direction::Direction;
 
 pub struct Neighbourhood {
     neighbours: Vec<Direction>
@@ -82,18 +56,6 @@ impl Neighbourhood {
 #[cfg(test)]
 mod tests {
     use super::{Direction, Neighbourhood};
-
-    #[test]
-    fn test_direction_to_offset() {
-        assert_eq!(Direction::Up.to_offset(), (0, -1));
-        assert_eq!(Direction::TopRight.to_offset(), (1, -1));
-        assert_eq!(Direction::Right.to_offset(), (1, 0));
-        assert_eq!(Direction::BottomRight.to_offset(), (1, 1));
-        assert_eq!(Direction::Down.to_offset(), (0, 1));
-        assert_eq!(Direction::BottomLeft.to_offset(), (-1, 1));
-        assert_eq!(Direction::Left.to_offset(), (-1, 0));
-        assert_eq!(Direction::TopLeft.to_offset(), (-1, -1));
-    }
 
     #[test]
     fn test_neighbourhood_moore() {
